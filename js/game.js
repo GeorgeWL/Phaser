3a1053beeaa9a3f9dfab16f13b44f00f06271589
George W Langham(Laptop) <georgelangham1990@gmail.com>
2017-06-11 00:16:59 +0100

    added collectable stars also a good point to call it a day.

--- js/game.js
+++ js/game.js
@@ -11,15 +11,17 @@ var ledge
 var player
 var cursors
 var hitPlatform
+var stars
+var star
 
-function preload() {
+function preload () {
   game.load.image('sky', '/assets/sky.png')
   game.load.image('ground', '/assets/platform.png')
   game.load.image('star', '/assets/star.png')
   game.load.spritesheet('dude', '/assets/dude.png', 32, 48)
 }
 
-function create() {
+function create () {
   // add arcade-style physics
   game.physics.startSystem(Phaser.Physics.ARCADE)
   // sky bg
@@ -52,6 +54,18 @@ function create() {
   // simple left and right animation for now
   player.animations.add('left', [0, 1, 2, 3], 10, true)
   player.animations.add('right', [5, 6, 7, 8], 10, true)
+  stars = game.add.group()
+  // add stars for scores with body
+  stars.enableBody = true
+  // create 12 spread out
+  for (var i = 0; i < 12; i++) {
+    // create a star inside of the group
+    star = stars.create(i * 70, 0, 'star')
+    // the magic of gravity
+    star.body.gravity.y = 6
+    // give each star a small random bounce
+    star.body.bounce.y = 0.7 + Math.random() * 0.2
+  }
 }
 
 function update () {
@@ -78,5 +92,13 @@ function update () {
   if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
     // jump (by gaining negative velocity)
     player.body.velocity.y = -350
+  }
+  // check if player collides
+  game.physics.arcade.collide(stars, platforms)
+  // check if player overlaps
+  game.physics.arcade.overlap(player, stars, collectStar, null, this)
+
+  function collectStar (player, star) {
+    star.kill()
   }
 }
